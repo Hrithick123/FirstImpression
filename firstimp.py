c9cf09afe3f43c57251b928e925156c0ef1b5942
HRITHICK SUNDAR J <hrithicksundar@gmail.com>
2025-03-03 14:34:42 +0530

    Update firstimp.py

--- firstimp.py
+++ firstimp.py
@@ -50,4 +50,5 @@ def chat():
         return jsonify({"error": f"An error occurred: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",4500))
+    app.run(host="0.0.0.0", port=port)
